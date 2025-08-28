c114e963106c5d328a4b7905d48b25280f73dcea
ASHUTOSHMEHTA <asahutoshmehta186@gmail.com>
2025-08-28 04:12:30 +0100

    container config done

---
+++ 01-basics-part-1/01-variables-and-values.js
@@ -0,0 +1,16 @@
+// .1 Variable -
+
+// A variable is a named storage in memory that holds a reference to some data. It acts like a container or label, allowing us to store, retrieve, and update values during program execution.
+
+// 2.Value -
+
+// A value is the actual piece of data stored inside a variable. It can be of different types such as number, string, boolean, object, etc. The value is what the program works with.
+
+// Example:
+
+let score = 95;
+let name = "Ashutosh";
+
+// score and name → variables (the containers / labels)
+
+// 95 and "Ashutosh" → values (the actual data inside them)
