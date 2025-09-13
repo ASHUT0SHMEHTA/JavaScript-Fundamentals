7b1b5426b156e96d0dee5a3567eb50de3c776c4a
ASHUTOSHMEHTA <asahutoshmehta186@gmail.com>
2025-09-13 15:04:06 +0530

    container config done

---
+++ 01-Basics/02-variables-and-values.js
@@ -0,0 +1,18 @@
+// What is a Variable in JavaScript?
+
+// A variable is a container used to store data in a program.
+// It allows us to save information and use it later.
+
+// Example:
+
+let name = "Rahul"; // 'name' is a variable
+
+// What are Values in JavaScript?
+
+// A value is the actual data stored inside a variable.
+// Values can be numbers, text (strings), true/false, objects, etc.
+
+// Example:
+
+let userName = "Rahul"; // the value is "Rahul"
+let age = 25; // the value is 25
